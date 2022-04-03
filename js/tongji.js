457325ee48f36fefc1e5aefca3a9c3c98f50bad1
RoundedOrange <RoundedOrange@outlook.com>
2022-04-04 00:54:11 +0800

    加入百度统计

---
+++ js/tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?cc048242b5d88a860789d33fd3a891ac";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
