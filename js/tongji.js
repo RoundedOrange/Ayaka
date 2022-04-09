f14d7d5f366c23ae963bdd020287e8f761b12b4b
RoundedOrange <RoundedOrange@outlook.com>
2022-04-09 19:00:43 +0800

    Update tongji.js

--- js/tongji.js
+++ js/tongji.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?cc048242b5d88a860789d33fd3a891ac";
+  hm.src = "https://hm.baidu.com/hm.js?fd5940fb05c53ce233d688d9272d1789";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
